78442d1c73c1ffec4cae83c58c54cf786f6e7dbf
Eugene Michasiw <eugene@michasiw.com>
2019-05-15 16:58:10 -0400

    form validation complete

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,6 +10,25 @@
 // 8. When the time remaining is equal to zero, play a sound, hide the countdown and display an end message and button.
 // 9. If the user presses the button, reset the application back to the start.
 
+(function ($) {
+  $.fn.inputFilter = function (inputFilter) {
+    return this.on("input keydown keyup mousedown mouseup select contextmenu drop", function () {
+      if (inputFilter(this.value)) {
+        this.oldValue = this.value;
+        this.oldSelectionStart = this.selectionStart;
+        this.oldSelectionEnd = this.selectionEnd;
+      } else if (this.hasOwnProperty("oldValue")) {
+        this.value = this.oldValue;
+        this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+      }
+    });
+  };
+}(jQuery));
+
+$(".number-input").inputFilter(function (value) {
+  return /^\d*$/.test(value) && (value === "" || parseInt(value) <= 999);
+});
+
 const mediApp = {};
 
 mediApp.meditationTime = 0;
@@ -18,14 +37,23 @@ mediApp.intervalTime = 0;
 mediApp.getInput = function() {
   $('form').on('submit', function(e) {
     e.preventDefault();
-    if ($('input[name=time]').val() === '' ) {
-      mediApp.meditationTime = $('input[name=time]').attr('placeholder');
-    } else { mediApp.meditationTime = $('input[name=time]').val(); }
+    const $time = $('input[name=time]');
+    const $interval = $('input[name=interval]');
+    $('.error').empty()
 
-    if ($('input[name=interval]').val() === '') {
-      mediApp.intervalTime = $('input[name=interval]').attr('placeholder');
-    } else { mediApp.intervalTime = $('input[name=interval]').val(); }  
-    console.log(mediApp.meditationTime, mediApp.intervalTime);
+    if ($time.val() === '' ) {
+      mediApp.meditationTime = Number($time.attr('placeholder'));
+    } else { mediApp.meditationTime = Number($time.val()); }
+
+    if ($interval.val() === '') {
+      mediApp.intervalTime = Number($interval.attr('placeholder'));
+    } else { mediApp.intervalTime = Number($interval.val()); }  
+    if (mediApp.intervalTime > mediApp.meditationTime) {
+      $('.error').html(`<p>Your interval time cannot be greater than your total time.</p>`)
+    } else {
+      console.log(mediApp.meditationTime, mediApp.intervalTime);
+    }
+      
   });
 }
 mediApp.init = function() {
